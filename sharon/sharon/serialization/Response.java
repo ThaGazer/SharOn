e313511b8f8ee261aae481b881bc8b7197f974d0
Justin Ritter <Justin_Ritter@baylor.edu>
2017-09-11 23:42:15 -0500

    added Response

---
+++ sharon/sharon/serialization/Response.java
@@ -0,0 +1,83 @@
+/*
+ * Response
+ * version 1.0 created 9/10/2017
+ *
+ * Authors:
+ * -Justin Ritter
+ */
+
+package sharon.serialization;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.List;
+
+/**
+ * Represents a SharOn response message
+ */
+public class Response extends Message {
+
+    /**
+     * Constructs new response with user input
+     * @param id message id
+     * @param ttl message TTL
+     * @param routingService message routing service
+     * @param sourceSharOnAddress message source address
+     * @param destinationSharOnAddress message destination address
+     * @param responseHost  Address and port of responding host
+     * @throws BadAttributeValueException if bad or null attribute value
+     */
+    public Response(byte[] id, int ttl, RoutingService routingService,
+            byte[] sourceSharOnAddress, byte[] destinationSharOnAddress,
+            InetSocketAddress responseHost)
+            throws BadAttributeValueException {
+
+    }
+
+    /**
+     * Constructs new response with deserialization
+     * @param in deserialization input source
+     * @throws IOException if I/O problem, including null
+     * @throws BadAttributeValueException if bad data value
+     */
+    public Response(MessageInput in)
+            throws IOException, BadAttributeValueException {
+
+    }
+
+    /**
+     * Get address and port of responding host
+     * @return responding host address and port
+     */
+    public InetSocketAddress getResponseHost() {
+
+    }
+
+    /**
+     * Set address and port of responding host
+     * @param responseHost responding host address and port
+     * @throws BadAttributeValueException if bad attribute value
+     */
+    public void setResponseHost(InetSocketAddress responseHost)
+            throws BadAttributeValueException{
+
+    }
+
+    /**
+     * Get list of results
+     * @return result list
+     */
+    public List<Result> getResultList() {
+
+    }
+
+    /**
+     * Add result to list
+     * @param result new result to add to result list
+     * @throws BadAttributeValueException
+     * if result is null or would make result list too long to encode
+     */
+    public void addResult(Result result) throws BadAttributeValueException {
+
+    }
+}
